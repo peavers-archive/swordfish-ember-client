13083d5e42976f26fcda0998503cb91821ddcf50
Chris Turner <peavers@gmail.com>
2018-01-07 10:51:27 +1300

    * tests added;

---
+++ tests/unit/mixins/secure-default-route-factory-test.js
@@ -0,0 +1,12 @@
+import EmberObject from '@ember/object';
+import SecureDefaultRouteFactoryMixin from 'swordfish-ember-client/mixins/secure-default-route-factory';
+import {module, test} from 'qunit';
+
+module('Unit | Mixin | secure default route factory');
+
+// Replace this with your real tests.
+test('it works', function (assert) {
+  let SecureDefaultRouteFactoryObject = EmberObject.extend(SecureDefaultRouteFactoryMixin);
+  let subject = SecureDefaultRouteFactoryObject.create();
+  assert.ok(subject);
+});
